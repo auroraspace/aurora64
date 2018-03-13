a66a30dbe9054ac1eeed49d43110616477bd6f88
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2018-03-13 17:19:37 -0300

    Adding contact section

---
+++ js/contact_me.js
@@ -0,0 +1,45 @@
+$(function() {
+
+    $("#contactForm").submit(function(event) {
+        event.preventDefault(); // prevent default submit behaviour
+        $.ajax({
+            url: "https://formspree.io/info@auroraspace.cl",
+            type: "POST",
+            data: $(this).serialize(),
+            dataType: "json",
+            cache: false,
+            beforeSend: function () {
+                // Sending message
+                $('#success').html("<div class='alert alert-info'>");
+                $('#success > .alert-info').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                $('#success > .alert-info').append("<strong>Sending...</strong>");
+                $('#success > .alert-info').append('</div>');
+            },
+            success: function () {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+                $('#success > .alert-success').append('</div>');
+
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            },
+            error: function () {
+                // Fail message
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                $('#success > .alert-danger').append("<strong>Ops! An error occurred. Please try again later!");
+                $('#success > .alert-danger').append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            }
+        })
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
